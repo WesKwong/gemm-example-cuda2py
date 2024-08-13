87483a030a3b36722a86d9bf50beeefdefc9354c
WesKwong <weskwong@outlook.com>
2024-08-13 13:38:07 +0800

    rewrite naive numba to a more naive version

--- numba_mm/naive_numba_matmul.py
+++ numba_mm/naive_numba_matmul.py
@@ -3,20 +3,20 @@ from numba import cuda
 
 @cuda.jit("void(float32[:,:], float32[:,:], float32[:,:],"
           "int64, int64, int64)")
-def naive_numba_matmul(out, a, b, M, N, K):
+def naive_numba_matmul(c, a, b, M, N, K):
     id_x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
     id_y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
-    if id_x > N or id_y > M:
+    if id_x > M or id_y > N:
         return
 
     res = 0
     for k in range(K):
-        res += a[id_y, k] * b[k, id_x]
-    out[id_y, id_x] = res
+        res += a[id_x, k] * b[k, id_y]
+    c[id_x, id_y] = res
 
 
-def launch_naive_numba_matmul(out, a, b, M, N, K, BLOCK_SIZE):
+def launch_naive_numba_matmul(c, a, b, M, N, K, BLOCK_SIZE):
     block = (BLOCK_SIZE, BLOCK_SIZE)
-    grid = ((N + BLOCK_SIZE - 1) // BLOCK_SIZE,
-            (M + BLOCK_SIZE - 1) // BLOCK_SIZE)
-    naive_numba_matmul[grid, block](out, a, b, M, N, K, BLOCK_SIZE)
+    grid = ((M + BLOCK_SIZE - 1) // BLOCK_SIZE,
+            (N + BLOCK_SIZE - 1) // BLOCK_SIZE)
+    naive_numba_matmul[grid, block](c, a, b, M, N, K)
